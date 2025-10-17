d21013f8c00ecbcf34fa68b24ce09d9fc0f5b1cd
BarudinXploit <holilhsyhd@gmail.com>
2025-10-17 10:40:56 +0700

    Delete client/public/src directory

--- client/public/src/App.jsx
+++
@@ -1,9 +0,0 @@
-
-import React, { useState } from "react";
-import Login from "./Login";
-import Dashboard from "./Dashboard";
-
-export default function App(){
-  const [authed, setAuthed] = useState(false);
-  return authed ? <Dashboard /> : <Login onSuccess={() => setAuthed(true)} />;
-}
